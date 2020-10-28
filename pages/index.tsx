04c101b758ca1452ed9a96089fd66aa7a23ef77c
Johann Perozo <johann.prz@gmail.com>
2020-10-27 23:55:14 -0400

    Adds editorconfig

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,11 @@ import styles from '../styles/home.module';
 
 const Home: NextPage = () => {
 
-    return (
-        <div className="home">
-            <p>Home</p>
-        </div>
-    );
+  return (
+    <div className="home">
+
+    </div>
+  );
 }
 
 export default Home;
