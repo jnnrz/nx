93b2c7147d95ac1864faa1fa9a4242182fbe01a9
Johann Perozo <johann.prz@gmail.com>
2020-10-27 23:17:17 -0400

    Adds Typescript

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+import styles from '../styles/home.module';
+
+const Home: NextPage = () => {
+
+    return (
+        <div className="home">
+            <p>Home</p>
+        </div>
+    );
+}
+
+export default Home;
