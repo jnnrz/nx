7e0d330338b0bab20235da8d51c0296753227564
Johann Perozo <johann.prz@gmail.com>
2020-10-28 23:31:54 -0400

    Adds auth0

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import type { AppProps } from 'next/app';
+import { Auth0Provider } from "@auth0/auth0-react";
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <Auth0Provider
+      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN}
+      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENTID}
+      redirectUri={process.env.NEXT_PUBLIC_AUTH0_REDIRECTURI}
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
+  );
 }
 
 export default MyApp;
