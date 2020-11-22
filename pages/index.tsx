0aaf2ec935711d7ea4d3665e8656a32b7210a100
Johann Perozo <johann.prz@gmail.com>
2020-11-21 22:24:45 -0400

    Adds navigation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import { NextPage } from "next";
 import styles from '../styles/home.module.scss';
+import Navigation from "../components/nav/Navigation";
 
 const Home: NextPage = () => {
-
   return (
-    <div className="home">
-    </div>
+    <>
+      <Navigation />
+      <div className="home">
+      </div>
+    </>
+
   );
 };
 
